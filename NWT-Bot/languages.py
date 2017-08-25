40d802402370f031930fed0a6f35de02aa34274b
Viva <august.gran@gmail.com>
2017-08-25 12:43:06 +0200

    v1.0
    Made another dictionary for URLs to access the different languages

---
+++ NWT-Bot/languages.py
@@ -0,0 +1,8 @@
+languageURL = {
+
+    'english' : 'https://wol.jw.org/en/wol/b/r1/lp-e/nwt/E/2013/',
+    'hebrew' : 'https://wol.jw.org/he/wol/b/r145/lp-q/sbi1/Q/2012/',
+    'greek' : 'https://wol.jw.org/el/wol/b/r11/lp-g/bi12/G/1997/',
+    'french' : 'https://wol.jw.org/fr/wol/b/r30/lp-f/Rbi8/F/1995/',
+    'spanish' : 'https://wol.jw.org/es/wol/b/r4/lp-s/Rbi8/S/1987/'
+    }
