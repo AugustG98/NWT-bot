ee806f1ac8be4f120fb8d701b7c0c425f4860cd3
Viva <august.gran@gmail.com>
2017-08-26 17:55:07 +0200

    changed a function name

--- NWT-Bot/client.py
+++ NWT-Bot/client.py
@@ -17,54 +17,54 @@ async def on_ready():
 @client.command(pass_context=True)
 async def english(ctx, book, chapterANDverse):
     language = 'english'
-    header, verse, result = inputANDoutput(book, chapterANDverse, language)
+    header, verse, data = getdata(book, chapterANDverse, language)
 
-    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+result+'```')
+    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+data+'```')
 
 #HEBREW
 @client.command(pass_context=True)
 async def hebrew(ctx, book, chapterANDverse):
     language = 'hebrew'
-    header, verse, result = inputANDoutput(book, chapterANDverse, language)
+    header, verse, data = getdata(book, chapterANDverse, language)
 
-    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+result+'```')
+    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+data+'```')
 
 #GREEK
 @client.command(pass_context=True)
 async def greek(ctx, book, chapterANDverse):
     language = 'greek'
-    header, verse, result = inputANDoutput(book, chapterANDverse, language)
+    header, verse, data = getdata(book, chapterANDverse, language)
 
-    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+result+'```')
+    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+data+'```')
 
 #FRENCH
 @client.command(pass_context=True)
 async def french(ctx, book, chapterANDverse):
     language = 'french'
-    header, verse, result = inputANDoutput(book, chapterANDverse, language)
+    header, verse, data = getdata(book, chapterANDverse, language)
 
-    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+result+'```')
+    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+data+'```')
 
 #SPANISH
 @client.command(pass_context=True)
 async def spanish(ctx, book, chapterANDverse):
     language = 'spanish'
-    header, verse, result = inputANDoutput(book, chapterANDverse, language)
+    header, verse, data = getdata(book, chapterANDverse, language)
 
-    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+result+'```')
+    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+data+'```')
 
 #GERMAN
 @client.command(pass_context=True)
 async def german(ctx, book, chapterANDverse):
     language = 'german'
-    header, verse, result = inputANDoutput(book, chapterANDverse, language)
+    header, verse, data = getdata(book, chapterANDverse, language)
 
-    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+result+'```')
+    await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+data+'```')
 
-def inputANDoutput (book, chapterANDverse, language):
+def getdata(book, chapterANDverse, language):
 
     booknumber, chapter, verse, fromVerse, toVerse = InputHandler.process(book, chapterANDverse)
-    header, verse, result = OutputHandler.getVerses(booknumber, chapter, verse, fromVerse, toVerse, language)
-    return(header, verse, result)
+    header, verse, data = OutputHandler.getVerses(booknumber, chapter, verse, fromVerse, toVerse, language)
+    return(header, verse, data)
 
 client.run('MzQ2Nzg0MTI5OTIyMjM2NDI3.DHfORQ.U-ELYc67t1Sa0Mb1XMESc_bM9mA')
