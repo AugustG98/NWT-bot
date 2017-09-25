2e8531529674a23c846b68ea4198f8f94dd48d3c
Viva <16744145+Viva98@users.noreply.github.com>
2017-09-26 00:31:19 +0200

    Update client.py

--- NWT-Bot/client.py
+++ NWT-Bot/client.py
@@ -10,6 +10,7 @@ des = 'A discord bot for getting Bible verses from the New World Translation'
 prefix = '!'
 client = commands.Bot(description=des, command_prefix=prefix)
 online = '\n\nBot is online!\n\n'
+error = 'too many characters, 2000 characters max!'
 TooLong = '**The passage is too long for me to grab, sorry!**'
 Format = '**Wrong format used, try again:**\n```!language bookname chapter:verse(s)```'
 
@@ -17,9 +18,9 @@ Format = '**Wrong format used, try again:**\n```!language bookname chapter:verse
 async def on_ready():
     print(online)
 
-#PRINT SERVERS BOT IS CONNECTED TO IN CHAT
 @client.command(pass_context=True)
 async def servers(ctx):
+
     list = ''
     for server in client.servers:
         list += str(server)+',\n'
@@ -50,10 +51,74 @@ async def english(ctx, book, chapterANDverse, add='0'):
         await client.say(Format)
         pass
 
+#HEBREW
+@client.command(pass_context=True)
+async def english(ctx, book, chapterANDverse, add='0'):
+    language = 'hebrew'
+    try:
+        header, verse, data = getdata(book, chapterANDverse, language, add)
+        try:
+            await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+data+'```')
+
+        except Exception:
+            await client.say(TooLong)
+
+    except Exception:
+        await client.say(Format)
+        pass
+
+#GREEK
+@client.command(pass_context=True)
+async def english(ctx, book, chapterANDverse, add='0'):
+    language = 'greek'
+    try:
+        header, verse, data = getdata(book, chapterANDverse, language, add)
+        try:
+            await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+data+'```')
+
+        except Exception:
+            await client.say(TooLong)
+
+    except Exception:
+        await client.say(Format)
+        pass
+
+#FRENCH
+@client.command(pass_context=True)
+async def english(ctx, book, chapterANDverse, add='0'):
+    language = 'french'
+    try:
+        header, verse, data = getdata(book, chapterANDverse, language, add)
+        try:
+            await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+data+'```')
+
+        except Exception:
+            await client.say(TooLong)
+
+    except Exception:
+        await client.say(Format)
+        pass
+
+#SPANISH
+@client.command(pass_context=True)
+async def english(ctx, book, chapterANDverse, add='0'):
+    language = 'spanish'
+    try:
+        header, verse, data = getdata(book, chapterANDverse, language, add)
+        try:
+            await client.say('**'+header+':'+verse+' - New World Translation (NWT)**\n\n```css\n'+data+'```')
+
+        except Exception:
+            await client.say(TooLong)
+
+    except Exception:
+        await client.say(Format)
+        pass
+
 def getdata(book, chapterANDverse, language, add):
 
     booknumber, chapter, verse, fromVerse, toVerse = InputHandler.process(book, chapterANDverse, language, add)
     header, verse, data = OutputHandler.getVerses(booknumber, chapter, verse, fromVerse, toVerse, language)
     return(header, verse, data)
 
-client.run('MzQ2Nzg0MTI5OTIyMjM2NDI3.DKcsUA.9hMx0MsYIqbolZJuUVyIabsi87s')
+client.run('token')
