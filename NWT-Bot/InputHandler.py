5db6ada32cc22b762008444810e6c773030a52ef
Viva <16744145+Viva98@users.noreply.github.com>
2017-09-26 16:02:24 +0200

    Update InputHandler.py

--- NWT-Bot/InputHandler.py
+++ NWT-Bot/InputHandler.py
@@ -2,7 +2,7 @@ import books
 import time
 from datetime import datetime
 
-def process(book, Chapter_Verse, language, add):
+def Process(book, Chapter_Verse, language, add):
     if (add != '0'):
         book += Chapter_Verse
         Chapter_Verse = add
