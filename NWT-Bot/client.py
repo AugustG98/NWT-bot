f0c72dbb0121c3cdd1335fb1d2399e28e46562a6
Viva <16744145+Viva98@users.noreply.github.com>
2017-09-26 22:07:58 +0200

    removed a useless variable

--- NWT-Bot/client.py
+++ NWT-Bot/client.py
@@ -10,15 +10,15 @@ des = 'A discord bot for getting Bible verses from the New World Translation'
 prefix = '!'
 client = commands.Bot(description=des, command_prefix=prefix)
 online = '\n\nBot is online!\n\n'
-error = 'too many characters, 2000 characters max!'
 TooLong = '**The passage is too long for me to grab, sorry!**'
-Format = '**Wrong format used, try again:**\n```!language bookname chapter:verse(s)```'
+WrongFormat = '**Wrong format used, try again:**\n```!language bookname chapter:verse(s)```'
 
 @client.event
 async def on_ready():
     print(online)
     await client.change_presence(game=discord.Game(name='https://github.com/Viva98/NWT-Bot'))
 
+#SHOW SERVERS BOT IS CONNECTED TO IN CHAT
 @client.command(pass_context=True)
 async def servers(ctx):
 
@@ -27,7 +27,7 @@ async def servers(ctx):
         list += str(server)+',\n'
     await client.say('List of servers NWT-Bot is connected to: ```'+list+'```')
 
-#PRINT AVAILABLE LANGUAGES IN CHAT
+#SHOW AVAILABLE LANGUAGES IN CHAT
 @client.command(pass_context=True)
 async def language():
     available = ''
@@ -35,6 +35,19 @@ async def language():
         available += '\n!'+language
 
     await client.say('**Languages available:**```'+available+'```')
+
+#SHOW GITHUB LINK
+@client.command(pass_context=True)
+async def git():
+
+    await client.say('https://github.com/Viva98/NWT-Bot')
+
+#SHOW INVITE LINK
+@client.command(pass_context=True)
+async def invite():
+
+    await client.say('https://discordapp.com/oauth2/authorize?client_id=360576523062870016&scope=bot')
+
 
 #ENGLISH
 @client.command(pass_context=True)
@@ -77,7 +90,7 @@ def GetVerses(language, book, chapterANDverse, add):
             return client.say(TooLong)
 
     except Exception:
-        return client.say(Format)
+        return client.say(WrongFormat)
         pass
 
 def Handler(book, chapterANDverse, language, add):
